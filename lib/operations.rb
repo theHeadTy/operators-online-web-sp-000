e19d2239cef321de27018fa9ab9998b9c6301f48
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-02 22:30:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+  
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  elsif speed.between?(40, 60)
+    return false
+  end
 end
 
+ s1 = (speed > 60 or speed < 40) ? true : false 
 
+ s2 = speed.between?(40, 60)
 
-def not_safe?(speed)
-	
-end
-	
+ (true) ? s1 : s2
 
-
+not_safe?(61)
+not_safe?(39)
+not_safe?(45)
